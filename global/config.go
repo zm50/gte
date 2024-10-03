e0bb59c24d66e755f92f4899e34748c5882a1aba
root <root@x>
2024-10-03 10:42:56 +0800

    提供参数动态配置

--- global/config.go
+++ global/config.go
@@ -1,5 +1,13 @@
 package global
 
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+// ServerConfig gte框架内部配置
 type ServerConfig struct {
 	MaxConns int
 	MaxPacketSize int
@@ -11,6 +19,7 @@ type ServerConfig struct {
 	WorkersPerTaskQueue int
 }
 
+// Config gte框架默认配置
 var Config = &ServerConfig{
 	MaxConns: 1024,
 	MaxPacketSize: 4096,
@@ -22,4 +31,23 @@ var Config = &ServerConfig{
 	TaskQueues: 10,
 	TaskQueueLen: 100,
 	WorkersPerTaskQueue: 10,
+}
+
+// LoadConfig 从配置文件中加载配置
+func LoadConfig(filePath string) error {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		// 配置文件打开失败
+		fmt.Println("config file open failed")
+		return err
+	}
+
+	err = yaml.Unmarshal(data, Config)
+	if err != nil {
+		// 配置文件解析失败
+		fmt.Println("config file parse failed")
+		return err
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/go75/gte
 
 go 1.23.1
+
+require gopkg.in/yaml.v3 v3.0.1
